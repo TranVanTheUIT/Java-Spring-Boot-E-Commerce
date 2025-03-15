d370f554faac8b33a26c1d17b7efa7cefe8213c1
TranVanTheUIT <20520770@gm.uit.edu.vn>
2025-03-15 14:31:08 +0700

    develop user function

--- shopmeWebParent/shopmeBackEnd/src/main/java/com/shop/admin/user/UserController.java
+++ shopmeWebParent/shopmeBackEnd/src/main/java/com/shop/admin/user/UserController.java
@@ -1,10 +1,13 @@
 package com.shop.admin.user;
 
+import com.shop.commom.entity.Role;
 import com.shop.commom.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -19,6 +22,27 @@ public class UserController {
         model.addAttribute("listUsers", listUsers);
 
         return "users";
+    }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model) {
+
+        List<Role> listRoles = service.listRoles();
+        model.addAttribute("listRoles", listRoles);
+        User user = new User();
+        user.setEnabled(true);
+        model.addAttribute("user", user);
+
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        System.out.println(user);
+        service.save(user);
+        // message notify create user success
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+        return "redirect:/users";
     }
 
 }
--- shopmeWebParent/shopmeBackEnd/src/main/java/com/shop/admin/user/UserService.java
+++ shopmeWebParent/shopmeBackEnd/src/main/java/com/shop/admin/user/UserService.java
@@ -1,5 +1,6 @@
 package com.shop.admin.user;
 
+import com.shop.commom.entity.Role;
 import com.shop.commom.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,18 @@ public class UserService {
     @Autowired
     private  UserRepository userRepo;
 
+    @Autowired
+    private RoleRepository roleRepo;
+
     public List<User> listAll(){
         return (List<User>) userRepo.findAll();
+    }
+
+    public List<Role> listRoles(){
+        return (List<Role>) roleRepo.findAll();
+    }
+
+    public void save(User user) {
+        userRepo.save(user);
     }
 }
