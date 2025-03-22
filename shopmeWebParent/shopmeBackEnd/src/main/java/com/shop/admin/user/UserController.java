c49f403b5e4df85a919fcd2d358fde5b88b34c27
TranVanTheUIT <20520770@gm.uit.edu.vn>
2025-03-22 15:52:31 +0700

    code delete user

--- shopmeWebParent/shopmeBackEnd/src/main/java/com/shop/admin/user/UserController.java
+++ shopmeWebParent/shopmeBackEnd/src/main/java/com/shop/admin/user/UserController.java
@@ -62,4 +62,17 @@ public class UserController {
         }
     }
 
+    @GetMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable(name = "id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            service.delete(id);
+            redirectAttributes.addFlashAttribute("message", "The user ID " + id + " has been deleted successfully.");
+        } catch (UserNotFoundException e) {
+            redirectAttributes.addFlashAttribute("message", e.getMessage());
+
+        }
+
+        return "redirect:/users";
+    }
+
 }
